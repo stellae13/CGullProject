11cabea9b5d7644990336fa0ea1b7c4b6a647a8e
rleboeu <leboeufrm@gmail.com>
2023-10-08 13:19:27 -0400

    Added bundle models

---
+++ Models/Bundle.cs
@@ -0,0 +1,7 @@
+namespace CGullProject;
+
+public class Bundle {
+    public string BundleId { get; set; }
+    public decimal Discount { get; set; }
+    public string Name { get; set; }
+}
---
+++ Models/BundleItem.cs
@@ -0,0 +1,6 @@
+namespace CGullProject;
+
+public class BundleItem {
+    public string Id { get; set; }
+    public string ProductId { get; set; }
+}
