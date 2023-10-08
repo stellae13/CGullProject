80eaf1f3092286492178f73be6762251d61fd13c
rleboeu <leboeufrm@gmail.com>
2023-10-08 13:09:44 -0400

    UserId changed from String to Guid

--- Models/UserProductRelation.cs
+++ Models/UserProductRelation.cs
@@ -1,7 +1,7 @@
 namespace CGullProject;
 
 public class UserProductRelation {
-    public string UserId { get; set; } = "";
+    public Guid UserId { get; set; }
     public string ProductId { get; set; } = "";
     public int Quantity { get; set; }
 }
