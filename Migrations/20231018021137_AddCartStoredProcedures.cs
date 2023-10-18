58ea53b0b9c26547501b74516b2bffb57097733f
rleboeu <leboeufrm@gmail.com>
2023-10-17 22:27:52 -0400

    Created CartController, created stored procedures for CartController

---
+++ Migrations/20231018021137_AddCartStoredProcedures.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CGullProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCartStoredProcedures : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            string create_cart_details = $"CREATE OR ALTER PROC usp_GetCartDetails(@cartId int)" +
+                $"AS " +
+                $"SELECT CartItem.CartId, Product.Name, Product.Price AS 'UnitPrice', CartItem.Quantity, " +
+                $"Product.Price * CartItem.Quantity AS 'Subtotal'FROM CartItem " +
+                $"JOIN Product ON ProductId = Product.Id " +
+                $"WHERE CartItem.CartId = @cartId;";
+
+            string create_cart_totals = $"CREATE OR ALTER PROC usp_GetCartTotals(@cartId int) " +
+                $"AS " +
+                $"SELECT " +
+                $"SUM (Product.Price * CartItem.Quantity) AS 'RegularTotal'" +
+                $", SUM(Product.Price * CartItem.Quantity) AS 'BundleTotal'" +
+                $", SUM(Product.Price * CartItem.Quantity) * 1.07 AS 'TotalWithTax' " +
+                $"FROM CartItem " +
+                $"JOIN Product ON ProductId = Product.Id " +
+                $"WHERE CartItem.CartId = 1;";
+
+            migrationBuilder.Sql(create_cart_details);
+            migrationBuilder.Sql(create_cart_totals);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            string drop_cart_details = "drop proc usp_GetCartDetails;";
+            string drop_cart_totals = "drop proc usp_GetCartTotals;";
+
+            migrationBuilder.Sql(drop_cart_details);
+            migrationBuilder.Sql(drop_cart_totals);
+        }
+    }
+}
---
+++ Models/CartTotals.cs
@@ -0,0 +1,9 @@
+﻿namespace CGullProject.Models
+{
+    public class CartTotals
+    {
+        public decimal RegularTotal { get; set; }
+        public decimal BundleTotal { get; set; }
+        public decimal TotalWithTax { get; set; }
+    }
+}
